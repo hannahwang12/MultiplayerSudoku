340677fc87ab8c681b8546847af097cb56a0fb23
Hannah Wang <hannahwang12@gmail.com>
2018-02-28 00:15:46 -0500

    Add ability to check for incorrect and invalid input

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ for (var i = 0; i < 9; i++) {
 		var random = (Math.random() * 10);
 		if (random < 3) {
 			document.getElementById(currentGrid).innerHTML = solution[i][j];
-			document.getElementById(currentGrid).style.color = "#888";
+			document.getElementById(currentGrid).style.color = "#777";
 		} else {
 			puzzle[i][j] = 0;
 			var guess = document.createElement("INPUT");
@@ -87,16 +87,19 @@ function check() {
 			var currentGrid = "grid" + i + j;
 			var currentGuess = "guess" + i + j;
 			if (puzzle[i][j] == 0) {
-				var g = document.getElementById(currentGuess).value;
-				if (parseInt(g, 10) >= 1 && parseInt(g, 10) <= 9) {
-					guesses[i][j] = parseInt(g, 10);
+				var g = parseInt(document.getElementById(currentGuess).value, 10);
+				if (g >= 1 && g <= 9) {
+					guesses[i][j] = g;
 					console.log(guesses[i][j]);
 					console.log(solution[i][j]);
 					if (guesses[i][j] != solution[i][j]) {
 						document.getElementById(currentGrid).style.backgroundColor = "rgba(200, 0, 0, 0.3)"
+					} else if (guesses[i][j] == solution[i][j]) {
+						document.getElementById(currentGrid).style.backgroundColor = "transparent";
 					}
+				} else if (document.getElementById(currentGuess).value != "") {
+					document.getElementById(currentGrid).style.backgroundColor = "#aaa"
 				}
-				
 			}
 		}
 	}
