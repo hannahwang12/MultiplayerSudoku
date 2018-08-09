2fd6347e4e38c46f2e9bb526a26edd975dc734b1
Hannah Wang <hannahwang12@gmail.com>
2018-08-08 22:40:48 -0400

    update package.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-app.use(express.static('client'));
+app.use(express.static('src/client'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/client/index.html');
